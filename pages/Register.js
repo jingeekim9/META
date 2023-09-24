579a8737909a956da912d49cde371e8267c89631
Andrew Shin <andrewshin@Andrews-Air.attlocal.net>
2023-09-23 18:07:01 -0700

    firebase

--- pages/Register.js
+++ pages/Register.js
@@ -3,11 +3,82 @@ import { View, Text, TextInput } from "react-native";
 import { Input, Button } from '@rneui/themed';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword,  } from "firebase/auth";
+import { collection, doc, addDoc, getFirestore } from "firebase/firestore"; 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC55iBDd_uZhjnoxzVeNmnNg8bTDEXD2Fo",
+  authDomain: "meta-fc205.firebaseapp.com",
+  projectId: "meta-fc205",
+  storageBucket: "meta-fc205.appspot.com",
+  messagingSenderId: "313671883891",
+  appId: "1:313671883891:web:3ecf94acf648ee9ba85e06",
+  measurementId: "G-953P5N046G"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
 
 export default function Register() {
 
     const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('')
+    const [confirmPasswordError, setConfirmPasswordError] = useState('')
+    const [loading, setLoading] = useState(false);
+
+    const register = () => {
+        setLoading(true);
+
+        if(email == '')
+        {
+            setEmailError("Please input an email")
+            setLoading(false);
+        }
+        else if(password == '')
+        {
+            setPasswordError("Please input a password")
+            setLoading(false);
+        }
+        else
+        {
+            createUserWithEmailAndPassword(auth, email, password) // if this code runs successfully
+            .then((userCredential) => {
+                setLoading(false);
+                const user = userCredential.user;
+
+                addData();
+
+                props.navigation.reset({
+                    index: 0,
+                    routes: [{ name: "Login"}]
+                });
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                if(errorCode.includes("invalid-email")){
+                    setEmailError("Please enter a valid email");
+                }
+                else if(errorCode.includes("weak-password")){
+                    setPasswordError("Please enter a stronger password");
+                }
+                setLoading(false);
+            })
+        }
+        
+    }
+
+    const addData = async() => {
+        const docRef = await addDoc(collection(db, "Users"), {
+            email: email,
+            name: "temp"
+        });
+        console.log("Document written with ID: ", docRef.id);
+    }
 
     return (
         <View
@@ -108,6 +179,9 @@ export default function Register() {
                 width: hp(42),
                 marginLeft:hp(2),
                 height: hp(7)
+              }}
+              onPress={() => {
+                register();
               }}
             />
             
