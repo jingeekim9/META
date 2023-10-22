ee639681d554eded1b2c1ffb9a921b91175b60de
Andrew Shin <andrewshin@Andrews-Air.attlocal.net>
2023-10-21 18:04:20 -0700

    products page

--- pages/Products.js
+++ pages/Products.js
@@ -42,6 +42,11 @@ export default function Products() {
         },
     ])
 
+    const res = [];
+    for (let i = 0; i < products.length; i += 2) {
+        res.push(products.slice(i, i + 2));
+    }
+
     return (
         <View
             style={{
@@ -110,307 +115,67 @@ export default function Products() {
                             flexDirection: "row"
                         }}
                     >
-                        <Image
-                            style={{
-                                height: hp(20),
-                                width: hp(20),
-                                marginBottom: hp(1),
-                                marginLeft: hp(1)
-                            }}
-                            source={{
-                                uri: "https://m.media-amazon.com/images/I/61fOR8yqOmL._AC_SX679._SX._UX._SY._UY_.jpg"
-                            }}
-                        />
-                        <Image
-                            style={{
-                                height: hp(20),
-                                width: hp(20),
-                                marginLeft: hp(3)
-                            }}
-                            source={{
-                                uri: "https://m.media-amazon.com/images/I/61HunMUy6BL._AC_SX679._SX._UX._SY._UY_.jpg"
-                            }}
-                        />
+                        {
+                            res.map((el, ind) => (
+                                el.map((el2, ind2) => (
+                                    <View>
+                                        <Image
+                                            style={{
+                                                height: hp(20),
+                                                width: hp(20),
+                                                marginBottom: hp(1),
+                                                marginLeft: hp(1)
+                                            }}
+                                            source={{
+                                                uri: el2["url"]
+                                            }}
+                                        />
+                                        <Text
+                                            style={{
+                                                fontWeight: "600",
+                                                marginLeft: hp(2),
+                                            }}
+                                        >
+                                            {el2["name"]}
+                                        </Text>
+                                        <Text
+                                            style={{
+                                                marginLeft: hp(2)
+                                            }}
+                                        >
+                                            {el2["company"]}
+                                        </Text>
+                                        <Text
+                                            style={{
+                                                marginLeft: hp(2),
+                                                marginTop: hp(1),
+                                                fontWeight: "500",
+                                            }}
+                                        >
+                                            ${el2["price"]}
+                                        </Text>
+                                        
+                                    </View>
+                                ))
+                            ))
+                        }
                     </View>
 
-
-                    <View
-                        style={{
-                            flexDirection: "row",
-                            marginBottom: hp(5)
-                        }}
-                    >
-                        <View
-                            style={{
-                                flexDirection: "column",
-                                marginLeft: hp(2)
-                            }}
-                        >
-                            <Text
-                                style={{
-                                    fontWeight: "600",
-                                    marginLeft: hp(2),
-                                }}
-                            >
-                                INDIE
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2)
-                                }}
-                            >
-                                xcvb
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2),
-                                    marginTop: hp(1),
-                                    fontWeight: "500",
-                                }}
-                            >
-                                $9584
-                            </Text>
-                        </View>
-                        <View
-                            style={{
-                                flexDirection: "column",
-                                marginLeft: hp(16)
-                            }}
-                        >
-                            <Text
-                                style={{
-                                    fontWeight: "600",
-                                    marginLeft: hp(2),
-                                }}
-                            >
-                                FUNK
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2)
-                                }}
-                            >
-                                hjkl
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2),
-                                    marginTop: hp(1),
-                                    fontWeight: "500",
-                                }}
-                            >
-                                $2342
-                            </Text>
-                        </View>
-                    </View>
-
-                    <View
-                        style={{
-                            flexDirection: "row",
-                        }}
-                    >
-                        <Image
-                            style={{
-                                height: hp(19),
-                                width: hp(20),
-                                marginBottom: hp(1),
-                                marginLeft: hp(1)
-                            }}
-                            source={{
-                                uri: "https://m.media-amazon.com/images/I/714XSiO1mtL._AC_SX679._SX._UX._SY._UY_.jpg"
-                            }}
-                        />
-                        <Image
-                            style={{
-                                height: hp(19),
-                                width: hp(20),
-                                marginLeft: hp(3)
-                            }}
-                            source={{
-                                uri: "https://m.media-amazon.com/images/I/51znawVkEIL._AC_SX679._SX._UX._SY._UY_.jpg"
-                            }}
-                        />
-                    </View>
-
-
-                    <View
-                        style={{
-                            flexDirection: "row",
-                            marginBottom: hp(5)
-                        }}
-                    >
-                        <View
-                            style={{
-                                flexDirection: "column",
-                                marginLeft: hp(2)
-                            }}
-                        >
-                            <Text
-                                style={{
-                                    fontWeight: "600",
-                                    marginLeft: hp(2),
-                                }}
-                            >
-                                POP
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2)
-                                }}
-                            >
-                                ghjk
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2),
-                                    marginTop: hp(1),
-                                    fontWeight: "500",
-                                }}
-                            >
-                                $4859
-                            </Text>
-                        </View>
-                        <View
-                            style={{
-                                flexDirection: "column",
-                                marginLeft: hp(16)
-                            }}
-                        >
-                            <Text
-                                style={{
-                                    fontWeight: "600",
-                                    marginLeft: hp(2),
-                                }}
-                            >
-                                CLASSIC
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2)
-                                }}
-                            >
-                                asdfg
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2),
-                                    marginTop: hp(1),
-                                    fontWeight: "500",
-                                }}
-                            >
-                                $3029
-                            </Text>
-                        </View>
-                    </View>
-                    <View
-                        style={{
-                            flexDirection: "row"
-                        }}
-                    >
-                        <Image
-                            style={{
-                                height: hp(19),
-                                width: hp(20),
-                                marginBottom: hp(1),
-                                marginLeft: hp(1)
-                            }}
-                            source={{
-                                uri: "https://m.media-amazon.com/images/I/71QNbYasFjL._AC_SX679._SX._UX._SY._UY_.jpg"
-                            }}
-                        />
-                        <Image
-                            style={{
-                                height: hp(19),
-                                width: hp(20),
-                                marginLeft: hp(3)
-                            }}
-                            source={{
-                                uri: "https://m.media-amazon.com/images/I/61JOHamx0jL._AC_UX679_.jpg"
-                            }}
-                        />
-                    </View>
-
-
-                    <View
-                        style={{
-                            flexDirection: "row"
-                        }}
-                    >
-                        <View
-                            style={{
-                                flexDirection: "column",
-                                marginLeft: hp(2)
-                            }}
-                        >
-                            <Text
-                                style={{
-                                    fontWeight: "600",
-                                    marginLeft: hp(2),
-                                }}
-                            >
-                                MOZART
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2)
-                                }}
-                            >
-                                tyuio
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2),
-                                    marginTop: hp(1),
-                                    fontWeight: "500",
-                                }}
-                            >
-                                $23423
-                            </Text>
-                        </View>
-                        <View
-                            style={{
-                                flexDirection: "column",
-                                marginLeft: hp(15)
-                            }}
-                        >
-                            <Text
-                                style={{
-                                    fontWeight: "600",
-                                    marginLeft: hp(2),
-                                }}
-                            >
-                                VOLTAIRE
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2)
-                                }}
-                            >
-                                qwerty
-                            </Text>
-                            <Text
-                                style={{
-                                    marginLeft: hp(2),
-                                    marginTop: hp(1),
-                                    fontWeight: "500",
-                                }}
-                            >
-                                $1132
-                            </Text>
-                        </View>
-                    </View>
                     <Text
                         style={{
-                            marginTop:hp(3),
-                            paddingTop:hp(1),
-                            fontSize:hp(2),
-                            borderColor:"black",
-                            borderRadius:hp(1),
-                            borderWidth:hp(0.1),
-                            width:"92%",
-                            height:hp(5),
-                            alignSelf:"center",
-                            textAlign:"center"
+                            alignSelf: "center",
+                            textAlign: "center",
+
+                            width: "92%",
+                            height: hp(5),
+
+                            marginTop: hp(3),
+                            paddingTop: hp(1),
+
+                            fontSize: hp(2),
+                            borderColor: "black",
+                            borderRadius: hp(1),
+                            borderWidth: hp(0.1)
                         }}
                     >
                         Explore More ▼
