74d7581a1b5ce9e49feac4502a92fa86dc5fc6ff
Andrew Shin <andrewshin@Andrews-Air.attlocal.net>
2023-11-04 12:01:37 -0700

    home page companies

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Detail'>
+      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen
           name="Login"
           component={Login}
