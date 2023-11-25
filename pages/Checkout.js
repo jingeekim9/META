5df39c15dcbc740d0acc9402fcd7bed7a0658af5
Andrew Shin <andrewshin@Andrews-Air.attlocal.net>
2023-11-24 22:29:19 -0800

    checkout page

--- pages/Checkout.js
+++ pages/Checkout.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import { Button, Icon } from '@rneui/themed';
+import { Button, Icon, Input, AirbnbRating } from '@rneui/themed';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-import { collection, getDocs, where, query } from "firebase/firestore";
+import { collection, getDocs, where, query, addDoc } from "firebase/firestore";
 import Toast from "react-native-toast-message";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
 
 const firebaseConfig = {
     apiKey: "AIzaSyC55iBDd_uZhjnoxzVeNmnNg8bTDEXD2Fo",
@@ -21,10 +22,25 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-
-
 export default function Checkout({ route, navigation }) {
     const [products, setProducts] = useState([]);
+    const [company, setCompany] = useState('');
+    const [productName, setProductName] = useState('');
+    const [reviewText, setReviewText] = useState('');
+    const [rating, setRating] = useState(4);
+    const [loading, setLoading] = useState(false);
+    const sheetRef = useRef(null);
+
+    const addReview = async () => {
+        const docRef = await addDoc(collection(db, "Reviews"), {
+            rating: rating,
+            review: reviewText,
+            company: company,
+            productName: productName
+        });
+        setLoading(false);
+        console.log("Document written with ID: ", docRef.id);
+    }
 
     useEffect(() => {
         const getDatabase = async () => {
@@ -42,7 +58,8 @@ export default function Checkout({ route, navigation }) {
                     "price": data["price"],
                     "productImage": data["productImage"],
                     "productName": data["productName"],
-                    "size": data["size"]
+                    "size": data["size"],
+                    "companyName" : data["companyName"]
                 })
             });
             setProducts(tempArray)
@@ -116,9 +133,14 @@ export default function Checkout({ route, navigation }) {
                         </View>
                         :
                         products.map((el, ind) => (
-                            <View
+                            <TouchableOpacity
                                 style={{
                                     flexDirection: "row"
+                                }}
+                                onPress={() => {
+                                    sheetRef.current.snapToIndex(1);
+                                    setCompany(el["companyName"]);
+                                    setProductName(el["productName"]);
                                 }}
                                 key={ind}
                             >
@@ -128,8 +150,7 @@ export default function Checkout({ route, navigation }) {
                                         width: hp(10),
                                         marginTop: hp(1.5),
                                         marginLeft: hp(1),
-                                        borderRadius: hp(1),
-                                        backgroundColor:"red"
+                                        borderRadius: hp(1)
                                     }}
                                     source={{
                                         uri: el["productImage"]
@@ -137,17 +158,101 @@ export default function Checkout({ route, navigation }) {
                                 />
                                 <Text
                                     style={{
-                                        marginTop:hp(2),
-                                        marginLeft:hp(1.5)
+                                        marginTop: hp(2),
+                                        marginLeft: hp(1.5)
                                     }}
                                 >
                                     {el["productName"]} {"\n"}Size: {el["size"]} {"\n\n\n"}Price: {el["price"]}
                                 </Text>
-                            </View>
-                            
+                            </TouchableOpacity>
+
                         ))
                 }
             </ScrollView>
+            <BottomSheet
+                ref={sheetRef}
+                index={0}
+                snapPoints={["1%", "50%"]}
+                backgroundStyle={{
+                    backgroundColor:"#f4f4f4"
+                }}
+                
+            >
+                <View
+                    style={{
+                        flex:1,
+                        flexDirection:"column"
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontSize: hp(2.3),
+                            marginLeft: hp(1),
+                            marginTop: hp(1),
+                            letterSpacing: hp(0.1)
+                        }}
+                    >Leave a review!</Text>
+
+                    <View
+                        style={{
+                            marginTop:hp(3)
+                        }}
+                    >
+                        <AirbnbRating
+                            count={5}
+                            defaultRating={4}
+                            size={hp(3)}
+                            showRating={false}
+                            onFinishRating={(e) => {
+                                setRating(e);
+                            }}
+                        />
+                    </View>
+
+                    <Input
+                        placeholder="Type..."
+                        multiline={true}
+                        labelStyle={{
+                            marginLeft: hp(1),
+                        }}
+                        style={{
+                            marginTop: hp(3),
+                            fontWeight: "400"
+                        }}
+
+                        inputStyle={{
+                            color: "#686c6e"
+                        }}
+                        onChangeText={(txt) => {
+                            setReviewText(txt);
+                        }}
+                    />
+                    <Button
+                        title="Submit"
+                        titleStyle={{
+                            color: "white",
+                            fontWeight: "500"
+                        }}
+                        buttonStyle={{
+                            backgroundColor: "#2d3133",
+                            paddingVertical: hp(1.5)
+                        }}
+                        containerStyle={{
+                            borderRadius: hp(5),
+                            width: hp(42),
+                            marginLeft: hp(2),
+                            position:"absolute",
+                            bottom:hp(7)
+                        }}
+                        
+                        loading={loading}
+                        onPress={() => {
+                            setLoading(true);
+                            addReview();
+                        }}
+                    />
+                </View>
+            </BottomSheet>
         </View>
     );
 }
